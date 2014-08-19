adb2b5070a77e5e386922a3971d205c4e0f7a067
Victorious3 <Minefoto0@gmail.com>
2014-08-19 22:14:47 +0200

    Fixed the config, once again.

--- src/main/java/vic/mod/chat/Config.java
+++ src/main/java/vic/mod/chat/Config.java
@@ -28,7 +28,7 @@ public class Config
 		Configuration config = new Configuration(file);
 		config.load();
 		
-		modt = config.get("modt", "GENERAL", 		
+		modt = config.get("GENERAL", "modt",
 				"Welcome, \u00A7e\u00A7l%NAME%\u00A7r to \u00A7o%MODT%\u00A7r!/n"
 				+ "Currently, there are %ONLINE%/%ONLINE_MAX% players online./n"
 				+ "You are playing on dimension %DIM% (\u00A7o%DIM_NAME%\u00A7r)/n"
@@ -45,24 +45,24 @@ public class Config
 				+ "You can also use color codes, see http://minecraft.gamepedia.com/Formatting_codes.\n"
 				+ "/n is used for a line feed.\n").getString();
 		
-		modtEnabled = config.get("modt_enabled", "GENERAL", true, "Disable or enable the \"Message of the Day.\"").getBoolean(true);
-		nickEnabled = config.get("nick_enabled", "GENERAL", true, "Disable or enable the ability to choose a nickname via /nick").getBoolean(true);
-		channelListEnabled = config.get("channel_list_enabled", "GENERAL", true, "Disable or enable to show the list of joined channels when joining the server.").getBoolean(true);
-		nickPermissionLevel = config.get("nick_permlevel", "GENERAL", 3, "Change the permission level required to use the /nick command. 3 is OP by default.").getInt(3);
-		colorPermissionLevel = config.get("color_permlevel", "GENERAL", 3, "Change the permission level required to use chat formatting with \"&\" as prefix. 3 is OP by default.").getInt(3);
-		localEnabled = config.get("local_enabled", "GENERAL", true, "Disable or enable the local chat.").getBoolean(true);
-		localRange = config.get("local_range", "GENERAL", 50, "Change the block distance in which players receive the local chat.").getInt(50);
-		globalCrossDim = config.get("global_cross_dim", "GENERAL", true, "Enable if you want the global chat to be cross-dimensional.").getBoolean(true);
+		modtEnabled = config.get("GENERAL", "modt_enabled", true, "Disable or enable the \"Message of the Day.\"").getBoolean(true);
+		nickEnabled = config.get("GENERAL", "nick_enabled", true, "Disable or enable the ability to choose a nickname via /nick").getBoolean(true);
+		channelListEnabled = config.get("GENERAL", "channel_list_enabled", true, "Disable or enable to show the list of joined channels when joining the server.").getBoolean(true);
+		nickPermissionLevel = config.get("GENERAL", "nick_permlevel", 3, "Change the permission level required to use the /nick command. 3 is OP by default.").getInt(3);
+		colorPermissionLevel = config.get("GENERAL", "color_permlevel", 3, "Change the permission level required to use chat formatting with \"&\" as prefix. 3 is OP by default.").getInt(3);
+		localEnabled = config.get("GENERAL", "local_enabled", true, "Disable or enable the local chat.").getBoolean(true);
+		localRange = config.get("GENERAL", "local_range", 50, "Change the block distance in which players receive the local chat.").getInt(50);
+		globalCrossDim = config.get("GENERAL", "global_cross_dim", true, "Enable if you want the global chat to be cross-dimensional.").getBoolean(true);
 		
 		config.addCustomCategoryComment("STYLE", "Valid colors are: BLACK, DARK_BLUE, DARK_GREEN, DARK_AQUA, DARK_RED, DARK_PURPLE, GOLD, GRAY, DARK_GRAY, BLUE, GREEN, AQUA, RED, LIGHT_PURPLE, YELLOW, WHITE");
 		
-		colorHighlight = EnumChatFormatting.getValueByName(config.get("color_highlight", "STYLE", "DARK_AQUA", "The color used by the name hightlighting.").getString());
+		colorHighlight = EnumChatFormatting.getValueByName(config.get("STYLE", "color_highlight", "DARK_AQUA", "The color used by the name hightlighting.").getString());
 		if(colorHighlight == null) colorHighlight = EnumChatFormatting.DARK_AQUA;
 		
-		colorHighlightSelf = EnumChatFormatting.getValueByName(config.get("color_highlight_self", "STYLE", "DARK_RED", "The color used by the name hightlighting if yourself gets highlighted.").getString());
+		colorHighlightSelf = EnumChatFormatting.getValueByName(config.get("STYLE", "color_highlight_self", "DARK_RED", "The color used by the name hightlighting if yourself gets highlighted.").getString());
 		if(colorHighlightSelf == null) colorHighlightSelf = EnumChatFormatting.DARK_RED;
 		
-		urlPermissionLevel = config.get("url_permlevel", "GENERAL", 0, "Change the permission level required to post clickable links in chat. 0 is everyone by default.").getInt();
+		urlPermissionLevel = config.get("GENERAL", "url_permlevel", 0, "Change the permission level required to post clickable links in chat. 0 is everyone by default.").getInt();
 		
 		config.save();
 	}
